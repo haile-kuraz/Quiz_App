46f2b39d790bf388f4ba82f4ac3a9cf29699846b
Haileliul <hailetechlover@gmail.com>
2023-11-18 15:55:28 +0300

    update has been  made

--- quizapp/lib/Provider/AuthProvider.dart
+++ quizapp/lib/Provider/AuthProvider.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:provider/provider.dart';
 
+enum Status {
+  uninitialized,
+  authenticated,
+  authenticating,
+  authenticateError,
+  authenticateCancled,
+}
+
+class AuthProvider extends ChangeNotifier {
+  Status _status = Status.uninitialized;
+}
--- quizapp/lib/Util/Color.dart
+++ quizapp/lib/Util/Color.dart
@@ -1,1 +1,20 @@
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class colors {
+  static Future<Map<String, dynamic>> readJson(String name) async {
+    final String response = await rootBundle.loadString('Assets/tokens/$name');
+    final Map<String, dynamic> data = await json.decode(response);
+    return data["md"];
+  }
+
+  static Future<Map<String, dynamic>> getLightData() async {
+    return await readJson('LightTheme.json');
+  }
+
+  static Future<Map<String, dynamic>> getDarkData() async {
+    return await readJson('DarkTheme.json');
+  }
+}
